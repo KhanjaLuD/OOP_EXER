cec386d62fdbbc53d7fc6c7df1104107c993c94b
Admin <khanjalu.prv@gmail.com>
2022-09-17 20:33:37 +0700

    OOP1

--- src/OOP_1/Main_1.java
+++ src/OOP_1/Main_1.java
@@ -1,4 +1,56 @@
 package OOP_1;
+class Person{
+    private int id;
+    private String name;
+    private int age;
+    private String address;
 
+    public Person(){
+
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
 public class Main_1 {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setAddress("Ha Noi");
+        p.setAge(24);
+        p.setId(1001);
+        p.setName("Phuc");
+        System.out.println(p.getId());
+        System.out.println(p.getName());
+        System.out.println(p.getAge());
+        System.out.println(p.getAddress());
+    }
 }
